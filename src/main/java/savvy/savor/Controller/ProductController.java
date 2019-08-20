5c96a6a91b44f14828a112e203172b9d2c56072a
Joanna Hsiao <chsiao58@gmail.com>
2019-08-20 09:52:24 -0400

    move logic to service

--- src/main/java/savvy/savor/Controller/ProductController.java
+++ src/main/java/savvy/savor/Controller/ProductController.java
@@ -15,25 +15,15 @@ import savvy.savor.Service.ProductService;
 public class ProductController {
 
     private ProductService productService;
-    private NixRetriever nixRetriever;
 
     @Autowired
-    public ProductController(ProductService productService, NixRetriever nixRetriever) {
+    public ProductController(ProductService productService) {
         this.productService = productService;
-        this.nixRetriever = nixRetriever;
     }
 
     @GetMapping(value = "/{nixItemId}")
     private ResponseEntity<Product> show(@PathVariable String nixItemId) {
         Product found = productService.show(nixItemId);
-
-        // if not already in database, fetch data from Nix and store it in this database
-        if (found == null) {
-           ResponseEntity<Product> ogNixData = nixRetriever.exchange(
-                    "v2/search/item?nix_item_id="+nixItemId,
-                    HttpMethod.GET, Product.class);
-           return new ResponseEntity<>(productService.create(ogNixData.getBody()), HttpStatus.CREATED);
-        } else
-            return new ResponseEntity<>(found, HttpStatus.OK);
+        return new ResponseEntity<>(found, HttpStatus.OK);
     }
 }
--- src/main/java/savvy/savor/Service/ProductService.java
+++ src/main/java/savvy/savor/Service/ProductService.java
@@ -1,7 +1,11 @@
 package savvy.savor.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import savvy.savor.Http.NixRetriever;
 import savvy.savor.Model.Product;
 import savvy.savor.Repository.ProductRepository;
 
@@ -9,13 +13,26 @@ import savvy.savor.Repository.ProductRepository;
 public class ProductService {
 
     private final ProductRepository productRepo;
+    private NixRetriever nixRetriever;
+
 
     @Autowired
-    ProductService(ProductRepository productRepo){
+    ProductService(ProductRepository productRepo, NixRetriever nixRetriever){
         this.productRepo = productRepo;
+        this.nixRetriever = nixRetriever;
     }
 
     public Product show(String nixItemId) {
+        Product found = productRepo.findByNixItemId(nixItemId);
+        // if not already in database, fetch data from Nix and store it in this database
+        if (found == null) {
+            ResponseEntity<Product> ogNixData = nixRetriever.exchange(
+                    "v2/search/item?nix_item_id="+nixItemId,
+                    HttpMethod.GET, Product.class);
+            create(ogNixData.getBody());
+            return ogNixData.getBody();
+        } else
+
         return productRepo.findByNixItemId(nixItemId);
     }
 
