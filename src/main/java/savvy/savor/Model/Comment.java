ff8db1eee2802af0287595ad43b54c127e527b16
Joanna Hsiao <chsiao58@gmail.com>
2019-08-20 00:03:03 -0400

    use identity to store id rather than auto

--- src/main/java/savvy/savor/Model/Comment.java
+++ src/main/java/savvy/savor/Model/Comment.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public class Comment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long commentId;
 
     @ManyToOne
--- src/main/java/savvy/savor/Model/Recipe.java
+++ src/main/java/savvy/savor/Model/Recipe.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Recipe {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long recipeId;
 
     @ManyToOne
